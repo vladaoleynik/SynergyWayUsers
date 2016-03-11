3b82dc221395298e144f10ab30aafdae9b82115d
Vlada Oleynik <voleynik3221@gmail.com>
2016-03-11 15:29:25 +0200

    Directive for Menu bar

--- static/app/core/directives.js
+++ static/app/core/directives.js
@@ -2,8 +2,24 @@
   'use strict';
 
   angular.module('synergyWayUsers')
+    .directive('menuBar', menuBar)
     .directive('convertToNumber', convertToNumber)
     .directive('includeReplace', includeReplace);
+
+  function menuBar($location) {
+    return {
+      restrict: 'E',
+      templateUrl: '/static/app/shared/menu-bar/menu-bar.html',
+      link: function(scope, element, attrs) {
+        scope.menuClass = menuClass;
+
+        function menuClass(page) {
+          var current = $location.path().substring(1);
+          return page === current ? "active" : "";
+        }
+      }
+    };
+  }
 
   function convertToNumber() {
     return {
