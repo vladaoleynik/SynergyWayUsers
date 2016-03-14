011ea748b392c5b6a008cd8a3e2293484b790b04
Vlada Oleynik <voleynik3221@gmail.com>
2016-03-15 00:02:04 +0200

    Update create_db.sql

--- synergyWayUsers/db/create_db.sql
+++ synergyWayUsers/db/create_db.sql
@@ -1,6 +1,7 @@
+-- Creating user and DB
 CREATE USER synergy_way WITH PASSWORD 'root';
 
-CREATE DATABASE synergy_way_users
+CREATE DATABASE users
    WITH OWNER synergy_way
    TEMPLATE template0
    ENCODING 'SQL_ASCII'
@@ -9,4 +10,30 @@ CREATE DATABASE synergy_way_users
    LC_CTYPE  'C'
    CONNECTION LIMIT  -1;
 
-GRANT ALL PRIVILEGES ON DATABASE synergy_way_users TO synergy_way;
+GRANT ALL PRIVILEGES ON DATABASE users TO synergy_way;
+
+-- Connect to created DB
+\c users synergy_way;
+
+-- Creating DB schema
+CREATE TABLE "course" (
+  "course_id" serial NOT NULL PRIMARY KEY,
+  "name" varchar(80) NOT NULL,
+  "code" varchar(20) NOT NULL
+);
+
+CREATE TABLE "user" (
+  "user_id" serial NOT NULL PRIMARY KEY,
+  "name" varchar(100) NOT NULL,
+  "email" varchar(254) NOT NULL,
+  "mobile" varchar(20) NULL,
+  "phone" varchar(20) NULL,
+  "status" boolean NOT NULL DEFAULT FALSE
+);
+
+CREATE TABLE "user_courses" (
+  "id" serial NOT NULL PRIMARY KEY,
+  "user_id" integer NOT NULL,
+  "course_id" integer NOT NULL,
+  UNIQUE ("user_id", "course_id")
+);
