f7b0d646697663c2af9a25d803333317c22f6de5
Vlada Oleynik <voleynik3221@gmail.com>
2016-03-14 16:33:58 +0200

    Stored procedure to get paginating users data

---
+++ synergyWayUsers/db/procedures.sql
@@ -0,0 +1,43 @@
+\c users synergy_way;
+
+CREATE OR REPLACE FUNCTION fn_GetUserData (
+  _page_size INTEGER = NULL,
+  _page_number INTEGER = NULL
+)
+RETURNS TABLE (
+  "id" INTEGER,
+  "name" VARCHAR,
+  "email" VARCHAR,
+  "mobile" VARCHAR,
+  "phone" VARCHAR,
+  "status" BOOLEAN,
+  "full_count" BIGINT
+) AS
+$BODY$
+DECLARE page_number BIGINT;
+
+BEGIN
+ -- Construct paging parameter
+ IF (_page_size IS NOT NULL AND _page_number IS NOT NULL) THEN
+  page_number := (_page_size * (_page_number - 1));
+ END IF;
+
+ RETURN QUERY
+ SELECT
+  "user".user_id,
+  "user".name,
+  "user".email,
+  "user".mobile,
+  "user".phone,
+  "user".status,
+   COUNT(*) OVER() AS full_count
+ FROM public.user
+ ORDER BY "user".name
+ LIMIT _page_size
+ OFFSET page_number;
+
+ EXCEPTION WHEN OTHERS THEN
+ RAISE;
+END;
+$BODY$
+LANGUAGE 'plpgsql';
